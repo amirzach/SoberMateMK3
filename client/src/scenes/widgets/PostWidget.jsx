b2a29f91b65de70254ce971cb217e30a87da3a8e
Amir Zakariah <156103488+amirzach@users.noreply.github.com>
2025-01-20 19:55:22 +0800

    Add files via upload

--- client/src/scenes/widgets/PostWidget.jsx
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -4,7 +4,15 @@ import {
   FavoriteOutlined,
   ShareOutlined,
 } from "@mui/icons-material";
-import { Box, Divider, IconButton, Typography, useTheme } from "@mui/material";
+import { 
+  Box, 
+  Divider, 
+  IconButton, 
+  Typography, 
+  useTheme,
+  InputBase,
+  Button
+} from "@mui/material";
 import FlexBetween from "components/FlexBetween";
 import Friend from "components/Friend";
 import WidgetWrapper from "components/WidgetWrapper";
@@ -24,6 +32,7 @@ const PostWidget = ({
   comments,
 }) => {
   const [isComments, setIsComments] = useState(false);
+  const [comment, setComment] = useState("");
   const dispatch = useDispatch();
   const token = useSelector((state) => state.token);
   const loggedInUserId = useSelector((state) => state.user._id);
@@ -32,6 +41,7 @@ const PostWidget = ({
 
   const { palette } = useTheme();
   const main = palette.neutral.main;
+  const medium = palette.neutral.medium;
   const primary = palette.primary.main;
 
   const patchLike = async () => {
@@ -45,6 +55,26 @@ const PostWidget = ({
     });
     const updatedPost = await response.json();
     dispatch(setPost({ post: updatedPost }));
+  };
+
+  const handleComment = async () => {
+    const response = await fetch(
+      `http://localhost:5000/posts/${postId}/comment`,
+      {
+        method: "PATCH",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          userId: loggedInUserId,
+          comment: comment
+        }),
+      }
+    );
+    const updatedPost = await response.json();
+    dispatch(setPost({ post: updatedPost }));
+    setComment("");
   };
 
   return (
@@ -87,18 +117,79 @@ const PostWidget = ({
             <Typography>{comments.length}</Typography>
           </FlexBetween>
         </FlexBetween>
+
+        <IconButton>
+          <ShareOutlined />
+        </IconButton>
       </FlexBetween>
+
       {isComments && (
         <Box mt="0.5rem">
           {comments.map((comment, i) => (
-            <Box key={`${name}-${i}`}>
+            <Box key={`${comment.userId}-${i}`}>
               <Divider />
-              <Typography sx={{ color: main, m: "0.5rem 0", pl: "1rem" }}>
-                {comment}
-              </Typography>
+              <Box p="1rem" display="flex" alignItems="flex-start" gap="1rem">
+                <img
+                  style={{
+                    objectFit: "cover",
+                    borderRadius: "50%",
+                    width: "45px",
+                    height: "45px",
+                  }}
+                  alt="user"
+                  src={`http://localhost:5000/assets/${comment.userPicturePath}`}
+                />
+                <Box>
+                  <Typography color={main} fontWeight="500">
+                    {`${comment.firstName} ${comment.lastName}`}
+                  </Typography>
+                  <Typography color={medium} fontSize="0.75rem">
+                    {comment.location}
+                  </Typography>
+                  <Typography color={main} sx={{ mt: "0.5rem" }}>
+                    {comment.comment}
+                  </Typography>
+                </Box>
+              </Box>
             </Box>
           ))}
           <Divider />
+          
+          {/* Comment Input Section */}
+          <Box p="1rem">
+            <FlexBetween gap="1.5rem">
+              <InputBase
+                placeholder="Write a comment..."
+                onChange={(e) => setComment(e.target.value)}
+                value={comment}
+                sx={{
+                  width: "100%",
+                  backgroundColor: palette.neutral.light,
+                  borderRadius: "2rem",
+                  padding: "1rem 2rem",
+                }}
+              />
+              <Button
+                disabled={!comment}
+                onClick={handleComment}
+                sx={{
+                  color: palette.background.alt,
+                  backgroundColor: palette.primary.main,
+                  borderRadius: "3rem",
+                  padding: "0.5rem 2rem",
+                  "&:hover": {
+                    backgroundColor: palette.primary.dark,
+                  },
+                  "&:disabled": {
+                    backgroundColor: palette.neutral.light,
+                    color: palette.neutral.main,
+                  },
+                }}
+              >
+                Post
+              </Button>
+            </FlexBetween>
+          </Box>
         </Box>
       )}
     </WidgetWrapper>
